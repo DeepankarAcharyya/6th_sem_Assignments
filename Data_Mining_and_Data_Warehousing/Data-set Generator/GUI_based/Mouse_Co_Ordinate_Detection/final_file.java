bfd058a06d81a49a55f95a521f47bd5c61fe9a5b
DeepankarAcharyya <deepankaracharyya@gmail.com>
2020-03-11 00:59:45 +0530

    structure changed

--- Data_Mining_and_Data_Warehousing/Data-set Generator/GUI_based/Mouse_Co_Ordinate_Detection/final_file.java
+++ Data_Mining_and_Data_Warehousing/Data-set Generator/GUI_based/Mouse_Co_Ordinate_Detection/final_file.java
@@ -1,23 +1,32 @@
-
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
 //for mouse-event-handling
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 /*
-                            OBJECTIVES
---- JFrame is displayed
---- display the co-ordinates only when thte mouse is clicked
---- display the points
+                                    OBJECTIVES
+--- JFrame is displayed---------------------------------------------------------Done
+--- display the co-ordinates only when thte mouse is clicked--------------------Done
+--- display the clicked points
 */
-class myMouseListener implements MouseListener{
-     
+
+class final_file extends JFrame implements MouseListener{
+
+ final_file(){
+    JFrame frame =new JFrame("Data-set Frame");
+       
+    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    frame.addMouseListener(this);
+
+    int width=500, height=500;
+    frame.setSize(width, height);
+    frame.setVisible(true);
+ }
+
  public void mouseClicked(MouseEvent event){  
     System.out.println("x = "+ event.getX() + " y = " + event.getY());      
- }
+    drawPoints(event.getX(),event.getY());
+}
 
  public void mouseEntered(MouseEvent event){}
 
@@ -26,23 +35,18 @@ class myMouseListener implements MouseListener{
  public void mousePressed(MouseEvent event){}
 
  public void mouseReleased(MouseEvent e){
-    // Graphics g=getGraphics();  
-    // g.setColor(Color.BLUE);  
-    // g.fillOval(e.getX(),e.getY(),10,10);
- }
-}
-
-
-public class final_file {
     
-   public static void main(String[] args){
-       JFrame frame =new JFrame("Data-set Frame");
-       
-       frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-       frame.addMouseListener(new myMouseListener());
+ }
 
-       int width=500, height=500;
-       frame.setSize(width, height);
-       frame.setVisible(true);
-   }
+ public void drawPoints(int x,int y){
+    Graphics g=getGraphics();
+    g.drawOval(x,y,x,y);  
+    g.setColor(Color.BLUE);  
+    g.fillOval(x,y,10,10); 
+ }
+
+ public static void main(String[] args){
+    new final_file();
+    }
+
 }
